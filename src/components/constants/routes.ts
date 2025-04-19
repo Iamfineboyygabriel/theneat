c76fd74c68cdcbe4d5870f93ce39747936990997
iamfineboyygabriel <tundegabriel999@gmail.com>
2025-04-19 22:53:19 +0100

    fixes

--- src/components/constants/routes.ts
+++ src/components/constants/routes.ts
@@ -1,8 +1,8 @@
 export const ROUTES = {
   HOME: "/",
-  SERVICES: "/services",
-  CONTACT_US: "/contact-us",
-  AREAS: "/areas",
-  ABOUT_US: "/about-us",
-  LOCATION_DETAILS: '/areas/location-details'
+  SERVICES: "services",
+  CONTACT_US: "contact-us",
+  AREAS: "areas",
+  ABOUT_US: "about-us",
+  LOCATION_DETAILS: 'areas/location-details'
 } as const;
